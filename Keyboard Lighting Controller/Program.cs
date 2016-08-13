ab5a26c0bff2f3e1faa1379e9feea1523c42817e
Doug <dougie+github@freshprogramming.com>
2016-08-13 07:19:06 -0700

    Added ConvertKeyToCorsairKeyID

--- Keyboard Lighting Controller/Program.cs
+++ Keyboard Lighting Controller/Program.cs
@@ -62,7 +62,7 @@ namespace Keyboard_Lighting_Controller
 
             TestKeyboardLights();
 
-            UnhookWindowsHookEx(_hookID);
+            //UnhookWindowsHookEx(_hookID);
             Console.WriteLine("unhooked");
 
         }
@@ -72,18 +72,138 @@ namespace Keyboard_Lighting_Controller
 
         private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            Console.WriteLine("Code="+ nCode);
+            Console.WriteLine("Code=" + nCode);
 
             if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
             {
                 int vkCode = Marshal.ReadInt32(lParam);
-                Console.WriteLine((Keys)vkCode);
+
+                CorsairKeyboardKeyId kID = ConvertKeyToCorsairKeyID((Keys)vkCode);
+
+                Console.WriteLine((Keys)vkCode + " - "+kID);
 
                 //StreamWriter sw = new StreamWriter(Application.StartupPath + @"\log.txt", true);
                 //sw.Write((Keys)vkCode);
                 //sw.Close();
             }
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static CorsairKeyboardKeyId ConvertKeyToCorsairKeyID(Keys k)
+        {
+            switch (k)
+            {
+                case Keys.Oemtilde: return CorsairKeyboardKeyId.GraveAccentAndTilde;
+                case Keys.D1: return CorsairKeyboardKeyId.D1;
+                case Keys.D2: return CorsairKeyboardKeyId.D2;
+                case Keys.D3: return CorsairKeyboardKeyId.D3;
+                case Keys.D4: return CorsairKeyboardKeyId.D4;
+                case Keys.D5: return CorsairKeyboardKeyId.D5;
+                case Keys.D6: return CorsairKeyboardKeyId.D6;
+                case Keys.D7: return CorsairKeyboardKeyId.D7;
+                case Keys.D8: return CorsairKeyboardKeyId.D8;
+                case Keys.D9: return CorsairKeyboardKeyId.D9;
+                case Keys.D0: return CorsairKeyboardKeyId.D0;
+                case Keys.OemMinus: return CorsairKeyboardKeyId.MinusAndUnderscore;
+                case Keys.Oemplus: return CorsairKeyboardKeyId.EqualsAndPlus;
+                case Keys.Back: return CorsairKeyboardKeyId.Backspace;
+                case Keys.A: return CorsairKeyboardKeyId.A;
+                case Keys.B: return CorsairKeyboardKeyId.B;
+                case Keys.C: return CorsairKeyboardKeyId.C;
+                case Keys.D: return CorsairKeyboardKeyId.D;
+                case Keys.E: return CorsairKeyboardKeyId.E;
+                case Keys.F: return CorsairKeyboardKeyId.F;
+                case Keys.G: return CorsairKeyboardKeyId.G;
+                case Keys.H: return CorsairKeyboardKeyId.H;
+                case Keys.I: return CorsairKeyboardKeyId.I;
+                case Keys.J: return CorsairKeyboardKeyId.J;
+                case Keys.K: return CorsairKeyboardKeyId.K;
+                case Keys.L: return CorsairKeyboardKeyId.L;
+                case Keys.M: return CorsairKeyboardKeyId.M;
+                case Keys.N: return CorsairKeyboardKeyId.N;
+                case Keys.O: return CorsairKeyboardKeyId.O;
+                case Keys.P: return CorsairKeyboardKeyId.P;
+                case Keys.Q: return CorsairKeyboardKeyId.Q;
+                case Keys.R: return CorsairKeyboardKeyId.R;
+                case Keys.S: return CorsairKeyboardKeyId.S;
+                case Keys.T: return CorsairKeyboardKeyId.T;
+                case Keys.U: return CorsairKeyboardKeyId.U;
+                case Keys.V: return CorsairKeyboardKeyId.V;
+                case Keys.W: return CorsairKeyboardKeyId.W;
+                case Keys.X: return CorsairKeyboardKeyId.X;
+                case Keys.Y: return CorsairKeyboardKeyId.Y;
+                case Keys.Z: return CorsairKeyboardKeyId.Z;
+                case Keys.OemOpenBrackets: return CorsairKeyboardKeyId.BracketLeft;
+                case Keys.OemCloseBrackets: return CorsairKeyboardKeyId.BracketRight;
+                case Keys.OemBackslash: return CorsairKeyboardKeyId.Backslash;
+                case Keys.OemSemicolon: return CorsairKeyboardKeyId.SemicolonAndColon;
+                case Keys.OemQuotes: return CorsairKeyboardKeyId.ApostropheAndDoubleQuote;
+                case Keys.Oemcomma: return CorsairKeyboardKeyId.CommaAndLessThan;
+                case Keys.OemPeriod: return CorsairKeyboardKeyId.PeriodAndBiggerThan;
+                case Keys.OemQuestion: return CorsairKeyboardKeyId.SlashAndQuestionMark;
+                case Keys.Return: return CorsairKeyboardKeyId.Enter;
+                case Keys.Tab: return CorsairKeyboardKeyId.Tab;
+                case Keys.CapsLock: return CorsairKeyboardKeyId.CapsLock;
+                case Keys.LControlKey: return CorsairKeyboardKeyId.LeftCtrl;
+                case Keys.LShiftKey: return CorsairKeyboardKeyId.LeftShift;
+                case Keys.LWin: return CorsairKeyboardKeyId.LeftGui;
+                case Keys.LMenu: return CorsairKeyboardKeyId.LeftAlt;
+                case Keys.RMenu: return CorsairKeyboardKeyId.RightAlt;
+                case Keys.RControlKey: return CorsairKeyboardKeyId.RightCtrl;
+                case Keys.RShiftKey: return CorsairKeyboardKeyId.RightShift;
+                case Keys.RWin: return CorsairKeyboardKeyId.RightGui;
+                case Keys.Apps: return CorsairKeyboardKeyId.Application;
+                case Keys.Space: return CorsairKeyboardKeyId.Space;
+                case Keys.Escape: return CorsairKeyboardKeyId.Escape;
+                case Keys.F1: return CorsairKeyboardKeyId.F1;
+                case Keys.F2: return CorsairKeyboardKeyId.F2;
+                case Keys.F3: return CorsairKeyboardKeyId.F3;
+                case Keys.F4: return CorsairKeyboardKeyId.F4;
+                case Keys.F5: return CorsairKeyboardKeyId.F5;
+                case Keys.F6: return CorsairKeyboardKeyId.F6;
+                case Keys.F7: return CorsairKeyboardKeyId.F7;
+                case Keys.F8: return CorsairKeyboardKeyId.F8;
+                case Keys.F9: return CorsairKeyboardKeyId.F9;
+                case Keys.F10: return CorsairKeyboardKeyId.F10;
+                case Keys.F11: return CorsairKeyboardKeyId.F11;
+                case Keys.F12: return CorsairKeyboardKeyId.F12;
+                case Keys.PrintScreen: return CorsairKeyboardKeyId.PrintScreen;
+                case Keys.Scroll: return CorsairKeyboardKeyId.ScrollLock;
+                case Keys.Pause: return CorsairKeyboardKeyId.PauseBreak;
+                case Keys.Insert: return CorsairKeyboardKeyId.Insert;
+                case Keys.Home: return CorsairKeyboardKeyId.Home;
+                case Keys.PageUp: return CorsairKeyboardKeyId.PageUp;
+                case Keys.Delete: return CorsairKeyboardKeyId.Delete;
+                case Keys.End: return CorsairKeyboardKeyId.End;
+                case Keys.PageDown: return CorsairKeyboardKeyId.PageDown;
+                case Keys.Up: return CorsairKeyboardKeyId.UpArrow;
+                case Keys.Down: return CorsairKeyboardKeyId.DownArrow;
+                case Keys.Left: return CorsairKeyboardKeyId.LeftArrow;
+                case Keys.Right: return CorsairKeyboardKeyId.RightArrow;
+                case Keys.NumLock: return CorsairKeyboardKeyId.NumLock;
+                case Keys.Divide: return CorsairKeyboardKeyId.KeypadSlash;
+                case Keys.Multiply: return CorsairKeyboardKeyId.KeypadAsterisk;
+                case Keys.Subtract: return CorsairKeyboardKeyId.KeypadMinus;
+                case Keys.Add: return CorsairKeyboardKeyId.KeypadPlus;
+                //case Keys.Enter: return CorsairKeyboardKeyId.KeypadEnter;
+                case Keys.Decimal: return CorsairKeyboardKeyId.KeypadPeriodAndDelete;
+                case Keys.NumPad0: return CorsairKeyboardKeyId.Keypad0;
+                case Keys.NumPad1: return CorsairKeyboardKeyId.Keypad1;
+                case Keys.NumPad2: return CorsairKeyboardKeyId.Keypad2;
+                case Keys.NumPad3: return CorsairKeyboardKeyId.Keypad3;
+                case Keys.NumPad4: return CorsairKeyboardKeyId.Keypad4;
+                case Keys.NumPad5: return CorsairKeyboardKeyId.Keypad5;
+                case Keys.NumPad6: return CorsairKeyboardKeyId.Keypad6;
+                case Keys.NumPad7: return CorsairKeyboardKeyId.Keypad7;
+                case Keys.NumPad8: return CorsairKeyboardKeyId.Keypad8;
+                case Keys.NumPad9: return CorsairKeyboardKeyId.Keypad9;
+                case Keys.MediaPlayPause: return CorsairKeyboardKeyId.PlayPause;
+                case Keys.MediaPreviousTrack: return CorsairKeyboardKeyId.ScanPreviousTrack;
+                case Keys.MediaNextTrack: return CorsairKeyboardKeyId.ScanNextTrack;
+                case Keys.MediaStop: return CorsairKeyboardKeyId.Stop;
+                case Keys.VolumeMute: return CorsairKeyboardKeyId.Mute;
+                default: return CorsairKeyboardKeyId.Invalid;
+            }
         }
 
         private static IntPtr SetHook(LowLevelKeyboardProc proc)
